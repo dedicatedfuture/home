7303ee57a2a2b363dd2b1b0b91b470e7d7144e70
Jaimie Bertoli <jaimie.bertoli@gmail.com>
2021-10-22 15:30:56 -0400

    Updates

---
+++ precache-manifest.d2f869f8f2603e97046208e5908aba12.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5537e02ec8e31bc52ecc8507c658eb00",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "267712625b986383e8be",
+    "url": "/home/static/css/main.e16032d7.chunk.css"
+  },
+  {
+    "revision": "aeb8e664506b025fd573",
+    "url": "/home/static/js/2.42c2acec.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.42c2acec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "267712625b986383e8be",
+    "url": "/home/static/js/main.c2108766.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "f6dae0ccce3130161a36e6832fa854c7",
+    "url": "/home/static/media/afreebird.f6dae0cc.png"
+  },
+  {
+    "revision": "e21ad79bf6064043a2597fdc1af985cb",
+    "url": "/home/static/media/biopic.e21ad79b.png"
+  },
+  {
+    "revision": "80c178f46c11c6efe530d0066ec36631",
+    "url": "/home/static/media/jpmc.80c178f4.png"
+  },
+  {
+    "revision": "7a5c8e050833bc55233ea622f24502bb",
+    "url": "/home/static/media/resume.7a5c8e05.pdf"
+  }
+]);
